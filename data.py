7203376c49c890a23d4200eadc439e4496d5104f
13952522076 <xuma@my.unt.edu>
2021-05-11 15:47:45 -0400

    update

--- data.py
+++ data.py
@@ -23,6 +23,7 @@ def load_data(partition):
     all_data = []
     all_label = []
     for h5_name in glob.glob(os.path.join(DATA_DIR, 'modelnet40_ply_hdf5_2048', 'ply_data_%s*.h5'%partition)):
+        print(f"h5_name: {h5_name}")
         f = h5py.File(h5_name)
         data = f['data'][:].astype('float32')
         label = f['label'][:].astype('int64')
